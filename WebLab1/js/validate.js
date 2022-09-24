8d4352da681f2044c4b8c13f9f22be3ae445606b
Arslanka <ars2003lan@gmail.com>
2022-09-24 20:18:11 +0300

    Added js

--- WebLab1/js/validate.js
+++ WebLab1/js/validate.js
@@ -1,78 +1,63 @@
-function showError(input, message) {
-    // get the form-field element
+function checkX() {
+    const xCoordinatesArray = document.querySelectorAll('input[name="x[]"]:checked');
+    if (xCoordinatesArray.length === 1) {
+        return setSuccessFor(xCoordinatesArray[0]);
+    } else {
+        return setErrorFor(document.querySelectorAll('input[name="x[]"]')[0], "Please, select one checkbox");
+    }
+}
+
+function checkY() {
+    const yEl = document.getElementById("y");
+    const yVal = yEl.value
+    if (isNaN(parseFloat(yVal)) || yVal === "") {
+        return setErrorFor(yEl, "Please, input number");
+    } else if (parseFloat(yVal) < -3 || parseFloat(yVal) > 5) {
+        return setErrorFor(yEl, "Please, input number in specified range");
+    } else {
+        return setSuccessFor(yEl);
+    }
+}
+
+function checkR() {
+    const r = document.getElementById("r")
+    const rVal = r.value
+
+    if (isNaN(parseFloat(rVal)) || rVal === "") {
+        return setErrorFor(r, "Please, input number");
+    } else if (parseFloat(rVal) < 1 || parseFloat(rVal) > 4) {
+        return setErrorFor(r, "Please, input number in specified range");
+    } else {
+        return setSuccessFor(r);
+    }
+}
+
+function validateForm() {
+    let x = checkX()
+    let y = checkY();
+    let r = checkR();
+
+    return x && y && r;
+}
+
+function setSuccessFor(input) {
     const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('success');
-    formField.classList.add('error');
+    const small = formField.querySelector('small');
 
-    // show the error message
-    const error = formField.querySelector('small');
-    error.textContent = message;
+    small.innerText = "";
+
+    formField.className = 'form-field success';
+
+    return true;
 }
 
-function showSuccess(input) {
-    // get the form-field element
+function setErrorFor(input, message) {
     const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('error');
-    formField.classList.add('success');
+    const small = formField.querySelector('small');
 
-    // show the error message
-    const error = formField.querySelector('small');
-    error.textContent = '';
-}
+    small.innerText = message;
 
-function check_x() {
-    let valid = false;
+    formField.className = 'form-field error';
 
-    const xElements = document.querySelectorAll('input[name="x[]"]:checked');
-    if (xElements.length == 1) {
-        showSuccess(xElements[0]);
-        valid = true;
-    } else {
-        showError(document.querySelector('input[name="x[]"]'), "Please, select exactly 1 checkbox");
-        valid = false;
-    }
-    return valid;
-}
-
-function check_y() {
-    let valid = false;
-
-    const yElement = document.getElementById("y");
-    const y = yElement.value;
-    if (isNaN(y) || isNaN(parseFloat(y))) {
-        showError(yElement, "Please, enter number");
-        valid = false;
-    } else if (parseFloat(y) < -3 || parseFloat(y) > 5) {
-        showError(yElement, "Please, enter number in range {-3, 5}");
-        valid = false;
-    } else {
-        showSuccess(yElement);
-        valid = true;
-    }
-    return valid;
-}
-
-function check_r() {
-    let valid = false;
-
-    const rElement = document.getElementById("r");
-    const r = rElement.value;
-    if (isNaN(r) || isNaN(parseFloat(r))) {
-        showError(rElement, "Please, enter number");
-        valid = false;
-    } else if (parseFloat(r) < 1 || parseFloat(r) > 4) {
-        showError(rElement, "Please, enter number in range {1, 4}");
-        valid = false;
-    } else {
-        showSuccess(rElement);
-        valid = true;
-    }
-    return valid;
-}
-
-function validate_form() {
-    let isXValid = check_x(), isYValid = check_y(), isRValid = check_r();
-    return isXValid && isYValid && isRValid;
+    return false;
 }
